2a4f6e6b66a1f36ee6623dac50a2598237544178
CXN <804774355@qq.com>
2020-05-21 18:09:22 +0800

    s

---
+++ LearnProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnProject
+//
+//  Created by CXN on 2020/5/21.
+//  Copyright © 2020 chengning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
