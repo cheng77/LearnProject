8513ac5e425ae783d24f056a0cbb357b8413f979
CXN <804774355@qq.com>
2020-07-01 18:31:40 +0800

    ss

---
+++ LearnProject/Person.m
@@ -0,0 +1,15 @@
+//
+//  Person.m
+//  LearnProject
+//
+//  Created by CXN on 2020/6/29.
+//  Copyright © 2020 chengning. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+- (void)run{
+    NSLog(@"%s",__func__);
+}
+@end
