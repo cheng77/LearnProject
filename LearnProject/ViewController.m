6ed8000002587223e7cb1e5eac3f8c92303e405f
CXN <804774355@qq.com>
2020-07-02 11:20:20 +0800

    ss

--- LearnProject/ViewController.m
+++ LearnProject/ViewController.m
@@ -19,5 +19,23 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)p_冒泡排序{
+    NSMutableArray * list = @[@(13),@(113),@(103),@(1),@(123),@(11),].mutableCopy;
+    for (NSInteger i = 0; i < list.count-1; i++) {
+        for (NSInteger j = 0; j < list.count-1-i;j++) {
+            if (list[j] < list[j+1]) {
+                NSInteger temp = [[NSString stringWithFormat:@"%@",list[j]] integerValue];
+                list[j] = list[j+1];
+                list[j+1] = @(temp);
+            }
+        }
+    }
+    
+    NSLog(@"list:%@",list);
+    
+}
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self p_冒泡排序];
+}
 @end
